013cba40bbe010cf2688ecb19f250bf5ac7e10c4
afrizaloky <afrizaloky@outlook.com>
2018-09-08 00:09:45 +0700

    first

--- project.py
+++ project.py
@@ -1,6 +1,42 @@
-print("Hello World")
+__author__      = "Afrizaloky"
+__copyright__   = "Copyright 2018, Indonesia"
 
-mobil = ['kijang', 'avanza', 'kodok']
+import numpy as np
+import numpy.linalg as linalg
 
-for i in mobil :
-    print(i)
+a = [[1,0,0],[0,1,0],[0,0,1]]
+b = [[20,10,30],[35,25,35],[50,10,20]]
+b_ = [10,20,10]
+
+## Multiply
+c = np.matmul(a,b)
+print(c)
+
+## Subtract
+c = np.subtract(a,b)
+print(c)
+
+## Add
+c = np.add(a,b)
+print(c)
+
+## Multiply
+c = np.divide(a,b)
+print(c)
+
+# Inverse
+c = np.linalg.inv(b)
+print(c)
+
+# Solving Equation
+c = np.linalg.solve(b, b_) ## b.c = b_
+print(c)
+d = np.allclose(np.dot(b, c), b_)
+print(d)
+
+# Transpose
+c = np.transpose(b)
+print(c)
+
+##
+c = np.
